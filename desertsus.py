3e3d42ea9cefc703528d05690799a47ac43f18ed
TheCrazy8 <bennettbashford@gmail.com>
2025-08-22 13:23:08 -0400

    Update desertsus.py

--- desertsus.py
+++ desertsus.py
@@ -1,1 +1,16 @@
+import time
+import random
+import tkinter as tk
 
+duration_seconds = 28800  # 8 hours
+start_time = time.time()  # Record the start time
+facing = 1 #value for direction of bus
+def veeroff():
+  direction = pickrandom(0,1)
+  if direction == 0:
+    
+
+
+while time.time() - start_time < duration_seconds:
+    
+    time.sleep(1)
